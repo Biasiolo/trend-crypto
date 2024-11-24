d092880499a35e916b534248bbc7d1322e30b41c
Biasiolo <biasiolorafael@gmail.com>
2024-11-24 02:34:45 -0300

    initial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { SiBitcoin } from "react-icons/si";
 import CryptoTrend from '../components/CryptoTrend';
 import Ranking from '../components/Ranking';
 import Analyze from '../components/Analyze';
@@ -7,15 +8,18 @@ import Analyze from '../components/Analyze';
 const HomePage = () => {
   return (
     <div className="container">
-        <div className="bg-dark text-white p-4 rounded">
-        <h1 >Welcome to Trend Crypto</h1>
+      <div className="bg-dark text-white text-center p-4 rounded">
+        <h1>
+          <SiBitcoin className="me-2" />  
+          Welcome to Trend Crypto
+          <SiBitcoin className="ms-2" /> 
+        </h1>
         <p>Analyze rapid crypto trends for trading decisions.</p>
-        </div>
-      
+      </div>
+
       <Ranking />
       <Analyze />
       <CryptoTrend />
-      
     </div>
   );
 };
