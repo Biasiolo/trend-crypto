020fbb47971ee74637d6f6cbe2a10418e094c8ae
Biasiolo <biasiolorafael@gmail.com>
2024-11-26 02:00:35 -0300

    ajustes2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -77,7 +77,7 @@ const HomePage = () => {
     <div className="container mt-0">
       {/* Header com Link para Home */}
       <div
-        className="header bg-dark text-white text-center p-5 mt-0 rounded-bottom"
+        className="header bg-dark text-white text-center p-5 mt-0 rounded"
         style={{ cursor: 'pointer' }}
         onClick={() => setActiveComponent('futures')}
       >
